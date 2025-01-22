a65b2e0b0be13de9e38c741c14ee8adeb037300f
joc59 <7039571+joc59@users.noreply.github.com>
2025-01-22 11:43:31 +0100

    Use actual power from secondary.fit in RecordMesg

--- PowerCalculator.cs
+++ PowerCalculator.cs
@@ -2,9 +2,25 @@ using Dynastream.Fit;
 
 class PowerCalculator
 {
+    // we don't return a power value if the closests is more than 3 seconds apart
+    private const int MAX_TIMESTAMP_DIFF_IN_SECONDS = 3;
+    private readonly List<RecordMesg> powerRecords = [];
+
     public event MesgEventHandler? MesgEvent;
 
-    public void OnMesg(Mesg message)
+    public void OnPowerMesg(Mesg message)
+    {
+        if (message.Num == 20) // Record message
+        {
+            RecordMesg recordMesg = new(message);
+            if (recordMesg.GetPower() != null)
+            {
+                powerRecords.Add(recordMesg);
+            }
+        }
+    }
+
+    public void OnMainMesg(Mesg message)
     {
         MesgEvent?.Invoke(this, new MesgEventArgs(AddPowerValues(message)));
     }
@@ -15,20 +31,62 @@ class PowerCalculator
         {
             case 20: // Record message
                 RecordMesg recordMesg = new(message);
-                recordMesg.SetPower(123);
+                RecordMesg? matchingPowerRecord = FindClosestPowerRecord(recordMesg.GetTimestamp());
+                if (matchingPowerRecord != null)
+                {
+                    recordMesg.SetPower(matchingPowerRecord.GetPower());
+                }
                 return recordMesg;
             case 19: // Lap message
                 LapMesg lapMesg = new(message);
-                lapMesg.SetAvgPower(123);
-                lapMesg.SetMaxPower(123);
+                lapMesg.SetAvgPower(GetAveragePower(lapMesg.GetStartTime(), lapMesg.GetTimestamp()));
+                lapMesg.SetMaxPower(GetMaxPower(lapMesg.GetStartTime(), lapMesg.GetTimestamp()));
                 return lapMesg;
             case 18: // Session message
                 SessionMesg sessionMesg = new(message);
-                sessionMesg.SetAvgPower(123);
-                sessionMesg.SetMaxPower(123);
+                sessionMesg.SetAvgPower(GetAveragePower(sessionMesg.GetStartTime(), sessionMesg.GetTimestamp()));
+                sessionMesg.SetMaxPower(GetMaxPower(sessionMesg.GetStartTime(), sessionMesg.GetTimestamp()));
                 return sessionMesg;
             default:
                 return message;
         }
+    }
+
+    private ushort? GetAveragePower(Dynastream.Fit.DateTime start, Dynastream.Fit.DateTime finish)
+    {
+        double? average = powerRecords
+            .FindAll((powerRecord) => powerRecord.GetTimestamp().GetTimeStamp() >= start.GetTimeStamp()
+                                        && powerRecord.GetTimestamp().GetTimeStamp() <= finish.GetTimeStamp())
+            .Average((powerRecord) => powerRecord.GetPower());
+        return average.HasValue ? Convert.ToUInt16(average.Value) : null;
+    }
+
+    private ushort? GetMaxPower(Dynastream.Fit.DateTime start, Dynastream.Fit.DateTime finish)
+    {
+        return powerRecords
+            .FindAll((powerRecord) => powerRecord.GetTimestamp().GetTimeStamp() >= start.GetTimeStamp()
+                                        && powerRecord.GetTimestamp().GetTimeStamp() <= finish.GetTimeStamp())
+            .Max((powerRecord) => powerRecord.GetPower());
+    }
+
+    private RecordMesg? FindClosestPowerRecord(Dynastream.Fit.DateTime dateTime)
+    {
+        RecordMesg? closestPowerRecord = powerRecords
+                    // Order by timestamp ascending
+                    .OrderBy(e => Math.Abs(e.GetTimestamp().GetTimeStamp()))
+                    // Find the latest record, which was just before the given timestamp
+                    .LastOrDefault(powerRecord => powerRecord.GetTimestamp().GetTimeStamp() <= dateTime.GetTimeStamp());
+
+        if (IsWithinBoundaries(closestPowerRecord, dateTime))
+        {
+            return closestPowerRecord;
+        }
+        return null;
+    }
+
+    private bool IsWithinBoundaries(RecordMesg? powerRecord, Dynastream.Fit.DateTime dateTime)
+    {
+        return powerRecord != null
+            && Math.Abs(powerRecord.GetTimestamp().GetTimeStamp() - dateTime.GetTimeStamp()) < MAX_TIMESTAMP_DIFF_IN_SECONDS;
     }
 }
--- Program.cs
+++ Program.cs
@@ -14,22 +14,32 @@ class Program
         string mergedFile = args[2];
 
         using var mainFileStream = new FileStream(mainFile, FileMode.Open);
+        using var secondaryFileStream = new FileStream(secondaryFile, FileMode.Open);
         using var destinationFileStream = new FileStream(mergedFile, FileMode.Create, FileAccess.ReadWrite);
-        Decode decode = new();
+
+        Decode decodeMain = new();
+        Decode decodeSecondary = new();
         Encode encode = new(ProtocolVersion.V20);
 
         PowerCalculator powerCalculator = new();
 
-        decode.MesgDefinitionEvent += (_, e) => encode.OnMesgDefinition(e.mesgDef);
-        decode.MesgEvent += (_, e) => powerCalculator.OnMesg(e.mesg);
+        // Recording power values from the secondary file
+        decodeSecondary.MesgEvent += (_, e) => powerCalculator.OnPowerMesg(e.mesg);
+
+        // Adding power values to the mesages from main file and passing them to the merged file
+        decodeMain.MesgEvent += (_, e) => powerCalculator.OnMainMesg(e.mesg);
         powerCalculator.MesgEvent += (_, e) => encode.OnMesg(e.mesg);
+
+        // Passing the mesg definitions to the main file to the merged file unaltered
+        decodeMain.MesgDefinitionEvent += (_, e) => encode.OnMesgDefinition(e.mesgDef);
 
         try
         {
+            decodeSecondary.Read(secondaryFileStream);
             encode.Open(destinationFileStream);
-            decode.Read(mainFileStream);
+            decodeMain.Read(mainFileStream);
         }
-        catch (Exception ex)
+        catch (FitException ex)
         {
             Console.WriteLine("A FitException occurred when trying to decode/encode FIT file. Message: " + ex.Message);
         }
